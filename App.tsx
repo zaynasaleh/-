ba360696ccdc1574280590938ec20bb7e959c73d
zaynasaleh <zmsaleh21@ad.just.edu.jo>
2026-02-27 02:40:00 +0300

    Fix import paths for Header and Footer components

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 
 import React, { useEffect, useState } from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import AddStoryPage from './pages/AddStoryPage';
