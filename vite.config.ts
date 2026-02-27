10e347a74c78f5900e71a9924ccd774e409548d4
zaynasaleh <zmsaleh21@ad.just.edu.jo>
2026-02-27 03:43:09 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
-  const basePath = process.env.BASE_PATH || '/';
+ const basePath = process.env.BASE_PATH || '/-/';
 
   return {
     base: basePath,
