e315bb61b56c961b043b3731a6401767bed210b1
zaynasaleh <zmsaleh21@ad.just.edu.jo>
2026-02-27 03:06:24 +0300

    Fix import path case for Footer component

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 
 import React, { useEffect, useState } from 'react';
 import Header from './components/Header';
-import Footer from './Components/Footer';
+import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import AddStoryPage from './pages/AddStoryPage';
